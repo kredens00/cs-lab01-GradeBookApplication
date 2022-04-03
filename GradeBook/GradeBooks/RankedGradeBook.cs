11536cfc2d92838b4cd1dfbd062b12cb6c5a8b48
skotara <80453773+kredens00@users.noreply.github.com>
2022-04-03 23:22:33 +0200

    Ranked Gradebook Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,16 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-        public class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
         {
-            public RankedGradeBook(string name) : base(name)
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count == 0)
             {
-                Type = GradeBookType.Ranked;
+                throw new InvalidOperationException();
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade < 60 && averageGrade >= 40)
+                return 'C';
+            else if (averageGrade < 40 && averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
             }
         }
-    
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+    }
 }
